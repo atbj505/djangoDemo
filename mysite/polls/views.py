1b005c61956b60046e5808d133fe495408f00eac
atbj505 <atbj505@hotmail.com>
2017-09-26 18:05:07 +0800

    template

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,18 +3,32 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import Http404
+from models import Question
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    # latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    # output = ','.join([q.question_text for q in latest_question_list])
+    # return HttpResponse(output)
+
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {
+        'latest_question_list': latest_question_list
+    }
+    return render(request, 'polls/index.html', context)
 
 
 def tribute(request):
-    return render(request, "tribute-page.html")
+    return render(request, "polls/tribute-page.html")
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist as e:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
 
 
 def results(request, question_id):
