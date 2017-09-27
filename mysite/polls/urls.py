24d8801bfb54627339253edd6f1075b9911bc6f4
atbj505 <atbj505@hotmail.com>
2017-09-27 14:23:04 +0800

    app_name

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -4,6 +4,7 @@
 from django.conf.urls import url
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
     url('^$', views.index, name='index'),
     url('^tribute/$', views.tribute, name='tribute'),
