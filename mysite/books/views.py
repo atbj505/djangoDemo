2a2704a9c2836c4d5849df5e81df3d6bcb06762b
atbj505 <atbj505@hotmail.com>
2017-10-24 11:03:13 +0800

    meta

--- mysite/books/views.py
+++ mysite/books/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render_to_response
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def index(request):
+    return HttpResponse('Hello')
+
+
+def meta(request):
+    template_name = 'books/meta.html'
+    return render_to_response(template_name,
+                              {'meta': sorted(request.META.items())})
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url('^polls/', include('polls.urls'))
+    url('^polls/', include('polls.urls')),
+    url('^books/', include('books.urls'))
 ]
