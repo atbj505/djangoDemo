33d05f70135aa10645815d7efc6e2c039878700b
atbj505 <atbj505@hotmail.com>
2017-10-27 16:10:49 +0800

    url improve

--- mysite/books/urls.py
+++ mysite/books/urls.py
@@ -10,4 +10,13 @@ urlpatterns = [
     url('^meta/$', views.meta, name='meta'),
     url('^search_form/$', views.search_form, name='search_form'),
     url('^search/$', views.search, name='search'),
+    url('^contact/$', views.contact, name='contact'),
+    url('^contact/thanks/$', views.contact_thanks, name='contact_thanks'),
+    url('^view1/$',
+        views.requires_login(views.my_view1),
+        name='requires_login'),
+    url('^view2/$',
+        views.requires_login(views.my_view2),
+        name='requires_login')
+
 ]
--- mysite/books/views.py
+++ mysite/books/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render_to_response
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from books.models import Book
-
-# Create your views here.
+from django.views.decorators.csrf import csrf_exempt
+from books.forms import ContactForm
 
 
 def index(request):
@@ -33,3 +33,40 @@ def search(request):
         else:
             errors.append('Please enter at most 20 characters.')
     return render_to_response('books/search_form.html', {'errors': errors})
+
+
+@csrf_exempt
+def contact(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            # cd = form.cleaned_data
+            return HttpResponseRedirect('/books/contact/thanks/')
+    else:
+        form = ContactForm(initial={'subject': 'I love your site!'})
+    return render_to_response('books/contact_form.html', {'form': form})
+
+
+@csrf_exempt
+def contact_thanks(request):
+    return HttpResponse('thanks')
+
+
+def requires_login(view):
+    def new_view(request, *args, **kwargs):
+        print('requires_login')
+        if not request.user.is_authenticated():
+            return HttpResponseRedirect('/accounts/login/')
+        return view(request, *args, **kwargs)
+
+    return new_view
+
+
+def my_view1(request, *args, **kwargs):
+    print('my_view1')
+    return HttpResponse('view1')
+
+
+def my_view2(request, *args, **kwargs):
+    print('my_view2')
+    return HttpResponse('view2')
